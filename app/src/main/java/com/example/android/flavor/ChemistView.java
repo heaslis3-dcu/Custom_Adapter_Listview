fdd239611ff8843f4e68651455e52ed8bae1cb28
SeanHeaslip <sean.j.heaslip@gmail.com>
2017-11-29 23:34:20 +0000

    onClick returning number position in Arraylist
    OnClick returns position in ArrayList of menu. TODO - update to return the string.

--- app/src/main/java/com/example/android/flavor/ChemistView.java
+++ app/src/main/java/com/example/android/flavor/ChemistView.java
@@ -15,31 +15,40 @@
  */
 package com.example.android.flavor;
 
-
+import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import org.w3c.dom.Text;
+
 import java.util.ArrayList;
+
+
 
 /**
  * {@link ChemistView} shows a list of Android platform releases.
  * For each release, display the name, version number, and image.
  */
-public class ChemistView extends AppCompatActivity {
-
-    private static CharSequence mToast = "You have pressed: ";
-    private static int mDuration = Toast.LENGTH_SHORT;
+public class ChemistView extends AppCompatActivity
+{
+    int mArrayPositionReturned = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.chemist_list_activ_main);
 
+
         // Create an ArrayList of AndroidFlavor objects
-        ArrayList<AndroidFlavor> androidFlavors = new ArrayList<AndroidFlavor>();
+        final ArrayList<AndroidFlavor> androidFlavors = new ArrayList<AndroidFlavor>();
         androidFlavors.add(new AndroidFlavor("Donut", "1.6", R.drawable.donut));
         androidFlavors.add(new AndroidFlavor("Eclair", "2.0-2.1", R.drawable.eclair));
         androidFlavors.add(new AndroidFlavor("Froyo", "2.2-2.2.3", R.drawable.froyo));
@@ -57,32 +66,79 @@ public class ChemistView extends AppCompatActivity {
         AndroidFlavorAdapter flavorAdapter = new AndroidFlavorAdapter(this, androidFlavors);
 
         // Get a reference to the ListView, and attach the adapter to the listView.
-        ListView listView = (ListView) findViewById(R.id.listview_flavor);
+       ListView listView = (ListView) findViewById(R.id.listview_flavor);
         listView.setAdapter(flavorAdapter);
+
+//        ListView lv = getListView();
+//        lv.setTextFilterEnabled(true);
+    //This returns the number in the arraylist.
+     listView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+     {
+         @Override
+         public void onItemClick(AdapterView<?> adapterView, View view, int i, long l)
+         {
+             Toast.makeText(getApplicationContext(), "Click ListItem Number " + i, Toast.LENGTH_SHORT).show();
+         }
+     });
+    }
+
+//        TextView listView1 = (TextView) findViewById(R.id.version_name);
+//        ArrayAdapter<AndroidFlavor> adapter = new ArrayAdapter<AndroidFlavor>(this, android.R.layout.simple_list_item_1,androidFlavors);
+//        listView1.setAdapter(adapter);
+       /*
+        TextView txtView = (TextView) findViewById(R.id.version_name);
+        txtView.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v)
+            {
+               // try{
+                    Toast.makeText(ChemistView.this, (CharSequence) androidFlavors.get(mArrayPositionReturned),Toast.LENGTH_SHORT).show();
+//                } catch
+//
+//            }
+//
+           }
+        });
+*/
+
+        /**
+         * https://developer.android.com/guide/topics/ui/declaring-layout.html#HandlingUserSelections
+         */
+        // Create a message handling object as an anonymous class.
+       /*
+        AdapterView.OnItemClickListener mMessageClickedHandler = new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView parent, View v, int position, long id) {
+                // Do something in response to the click
+                Toast.makeText(this, )
+            }
+        };
+
+        listView.setOnItemClickListener(mMessageClickedHandler);
+    */
     }
 
 
 
-    /*
-     * MAKE NOTE OF THIS FOR Debugging - first issue found after landscape.
-     *Citation:
-     * Code adapted from - under set-onclicklistener-with-linearlayout-imageview-textview
-     * https://stackoverflow.com/questions/29100224/
-     */
-    public void onClick(View view){
+/*
 
-//        switch(view.getId()){
-//            case R.id.list_item_icon:
-//            case R.id.version_name:
-//            case R.id.version_number:
-//            case R.id.linearLayoutID:
-                Toast toast = Toast.makeText(this, mToast, mDuration);
-                toast.show();
-//                break;
-//            default:
-//                break;
-//        }
+    public View getView(int position, View convertView, ViewGroup container) {
+        if (convertView == null) {
+            convertView = getLayoutInflater().inflate(R.layout.list_item, container, false);
+        }
 
+        ((TextView) convertView.findViewById(android.R.id.text1))
+                .setText(getItem(position));
+        return convertView;
     }
 
-}
+    private int getItem(int position)
+    {
+        return position;
+    }
+
+    public void onClick(View view) {
+        //Toast.makeText(this, getView(0, R.id.linearLayoutID,R.id.listview_flavor)).show();
+    }
+*/
+//}
