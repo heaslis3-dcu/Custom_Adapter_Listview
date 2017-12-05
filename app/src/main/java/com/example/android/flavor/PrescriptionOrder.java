09ac4a388d5eb9e8ed68977c296744c84fa2a40a
SeanHeaslip <sean.j.heaslip@gmail.com>
2017-12-05 22:26:36 +0000

    Testing Spinner
    Testing Spinner using Toast method to return user selection. Have slight issue in that the onCreate Method is running the onItemSelected method before Spinner is selected. Results in message with blank being toasted. Next Update to include onClickListner

--- app/src/main/java/com/example/android/flavor/PrescriptionOrder.java
+++ app/src/main/java/com/example/android/flavor/PrescriptionOrder.java
@@ -10,10 +10,12 @@ import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -31,18 +33,23 @@ import java.util.List;
  * https://software.intel.com/en-us/blogs/2014/03/25/capturing-handling-displaying-and-sharing-images-in-an-android-app
  */
 
-public class PrescriptionOrder extends AppCompatActivity
+public class PrescriptionOrder extends AppCompatActivity implements AdapterView.OnItemSelectedListener
 {
     EditText m_Name;
     EditText m_Message;
-    String TAG = "Assign3";
+    private static final String TAG = "Assign3";
     Uri m_ouputFileUri;
     private static final int REQUEST_SHARE =39714;
-    private File png = null;
     private File m_ImageFile;
     private Uri m_ImageUri;
     String imageFileName = "myImage.jpg";
-    String m_ImageFilePath;
+    String m_Spinner;
+    Spinner m_SpinnerClick;
+    Object m_Item;
+    String m_Time;
+    TextView mSpinnerText;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -51,26 +58,55 @@ public class PrescriptionOrder extends AppCompatActivity
         m_Name = (EditText) findViewById(R.id.edtTextTo);
         m_Message = (EditText) findViewById(R.id.edTxtDescription);
 
+
+        m_SpinnerClick = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(this,R.array.collectionTimes, android.R.layout.simple_spinner_item);
+        m_SpinnerClick.setAdapter(adapter);
+        m_SpinnerClick.setOnItemSelectedListener(this);
     }
+
+    /*
+    Citation: https://www.youtube.com/watch?v=aApS2W-j8oM (Slidenerd video 104 & 105)
+    Spinner Steps:
+    - Create the data source,
+    define the appearance layoutfile through which the adapter will put data inside the spinner
+    define what to do when the user clicks on the spinner using the OnItemSelectedListener
+     */
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l)
+    {
+        mSpinnerText = (TextView) view;
+        //Toast.makeText(this, "Selected: " + spinnerText.getText(), Toast.LENGTH_SHORT).show();
+        //return spinnerText;
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView)
+    {
+
+    }
+
 
    public void captureImage(View view)
    {
        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
        m_ImageFile = new File(Environment.getExternalStorageDirectory(),imageFileName);
-      m_ImageUri =  Uri.fromFile(m_ImageFile);
+       m_ImageUri =  Uri.fromFile(m_ImageFile);
 
        intent.putExtra(MediaStore.EXTRA_OUTPUT, m_ImageUri);
+       Log.i(TAG, "Camera has taken image.");
        startActivityForResult(intent, REQUEST_SHARE);//note number of request_share
    }
 
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
-        Log.i(TAG, "Enteres onActivityResult()");
+        Log.i(TAG, "Entering onActivityResult()");
         //Checking request for message:
         if(requestCode == REQUEST_SHARE) {
             if(resultCode == RESULT_OK) {
+                Log.i(TAG, "Image saved to filepath.");
                 Toast.makeText(this, "ImageFilePath is: " + m_ImageFile, Toast.LENGTH_LONG).show();
             } else {
                 Toast.makeText(this, "There was an error saving your accessing your image", Toast.LENGTH_SHORT).show();
@@ -82,7 +118,7 @@ public class PrescriptionOrder extends AppCompatActivity
     /**
      * Citation - Code modified from:
      * https://www.mkyong.com/android/android-spinner-drop-down-list-example/
-     */
+
 // adds items to spinner dynamically
 public void setHoursToSpinner() {
     //Spinner
@@ -91,10 +127,11 @@ public void setHoursToSpinner() {
     List<String> hour = new ArrayList<String>();
     ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
             android.R.layout.simple_spinner_item, hour);
+    Log.i(TAG, "Spinner activated.");
     adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
     spinner.setAdapter(adapter);
 }
-
+     */
     /*
      *Citiation: Reference:
       *  https://stackoverflow.com/questions/31445156
@@ -121,13 +158,19 @@ public void setHoursToSpinner() {
     }
     */
 
-
+    /**
+     * Citation: Code Modified from:
+     * https://stackoverflow.com/questions/13486814
+     * @param view
+     */
 
     public void openEmail(View view) {
+        Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
         Intent openEmailIntent = new Intent(Intent.ACTION_SEND);
         openEmailIntent.setType("text/html");
         openEmailIntent.putExtra(Intent.EXTRA_SUBJECT,
                         m_Name.getText().toString());
+        openEmailIntent.putExtra(Intent.EXTRA_TEXT, m_Spinner);
         //openEmailIntent.putExtra(Intent.E)
         openEmailIntent.putExtra(MediaStore.EXTRA_OUTPUT, m_ouputFileUri);
 //        openEmailIntent.putExtra(Intent.EXTRA_STREAM, m_ouputFileUri);
