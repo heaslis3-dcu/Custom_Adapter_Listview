aa8b2f80bacd1f283bb72e40906760cc47c80f5e
SeanHeaslip <sean.j.heaslip@gmail.com>
2017-12-05 20:23:52 +0000

    Update with camera and Toast to image path
    Camera working with toast confirming filepath to image.

--- app/src/main/java/com/example/android/flavor/PrescriptionOrder.java
+++ app/src/main/java/com/example/android/flavor/PrescriptionOrder.java
@@ -1,55 +1,48 @@
 package com.example.android.flavor;
 
+
 import android.content.Intent;
 import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
-import android.os.Parcelable;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+
 import java.io.File;
-import java.net.URI;
+
 import java.util.ArrayList;
+
 import java.util.List;
+
+
 
 /**
  * Created by seanh on 30/11/2017.
+ * 04/12/2017
+ * https://software.intel.com/en-us/blogs/2014/03/25/capturing-handling-displaying-and-sharing-images-in-an-android-app
  */
 
 public class PrescriptionOrder extends AppCompatActivity
 {
-    private Spinner spinner1;
-    private Button btnSpinner;
     EditText m_Name;
     EditText m_Message;
-    Spinner m_Time;
-    Image m_Photo;
-    File m_file;
-    Uri m_ouputFileUri;
-    String m_hour;
-//    String m_Subject = getString(R.string.Email_Subject);
-//    String m_ToEmail [] = {getString(R.string.ToEmail)};
-//    String m_FromEmail [] = {getString(R.string.FromEmail)};
-
-    /*
-    private static Intent sendPrescription;
-    private static final int RETURN_IMAGE = 2;
     String TAG = "Assign3";
-
-    EditText edTxtTo = (EditText) findViewById(R.id.edtTextTo);
-    EditText edTxtDesc = (EditText) findViewById(R.id.edTxtDescription);
-
-    */
+    Uri m_ouputFileUri;
+    private static final int REQUEST_SHARE =39714;
+    private File png = null;
+    private File m_ImageFile;
+    private Uri m_ImageUri;
+    String imageFileName = "myImage.jpg";
+    String m_ImageFilePath;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -57,41 +50,34 @@ public class PrescriptionOrder extends AppCompatActivity
         setContentView(R.layout.prescription);
         m_Name = (EditText) findViewById(R.id.edtTextTo);
         m_Message = (EditText) findViewById(R.id.edTxtDescription);
-       //m_Photo =
 
-
-        /* Original Spinner
-        final String[] collectionTime = {"1 Hr", "3 Hr", "6Hr","12 Hr","24 Hr","48 Hr"};
-        //Adapter used for spinner.
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_spinner_dropdown_item,
-                collectionTime);
-        spinner.setAdapter(adapter);
-
-
-
-        //Spinner Click Method;
-        spinner.setOnItemClickListener(new AdapterView.OnItemClickListener()
-        {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l)
-            {
-                m_hour = collectionTimes[i];
-                Toast.makeText(PrescriptionOrder.this, m_hour, Toast.LENGTH_SHORT).show();
-            }
-        });
-        */
-/*
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener()
-        {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l)
-            {
-                String item = androidFlavors.get(position).getVersionName();
-                Toast.makeText(getApplicationContext(), item, Toast.LENGTH_SHORT).show();
-            }
-        });*/
     }
+
+   public void captureImage(View view)
+   {
+       Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+       m_ImageFile = new File(Environment.getExternalStorageDirectory(),imageFileName);
+      m_ImageUri =  Uri.fromFile(m_ImageFile);
+
+       intent.putExtra(MediaStore.EXTRA_OUTPUT, m_ImageUri);
+       startActivityForResult(intent, REQUEST_SHARE);//note number of request_share
+   }
+
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        Log.i(TAG, "Enteres onActivityResult()");
+        //Checking request for message:
+        if(requestCode == REQUEST_SHARE) {
+            if(resultCode == RESULT_OK) {
+                Toast.makeText(this, "ImageFilePath is: " + m_ImageFile, Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "There was an error saving your accessing your image", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
 
     /**
      * Citation - Code modified from:
@@ -103,42 +89,27 @@ public void setHoursToSpinner() {
     Spinner spinner = (Spinner) findViewById(R.id.spinner);
     //Array used to hold Spinner data
     List<String> hour = new ArrayList<String>();
-   // hour.add("list 1");
-    //hour.add("list 2");
-    //hour.add("List 3");
-
     ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
             android.R.layout.simple_spinner_item, hour);
     adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
     spinner.setAdapter(adapter);
 }
 
-public void addListenerOnSpinner() {
-
-}
-
-//get the selected dropdown list value
     /*
-    public void addListenerOnButton() {
-        spinner1 = (Spinner) findViewById(R.id.spinner);
-
-
-        spinner1.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View view)
-            {
-                Toast.makeText(PrescriptionOrder.this,
-                        "OnClickListener : " +
-                "Spinner 1 : " + String.valueOf(spinner1.getSelectedItem()),Toast.LENGTH_SHORT).show();
-            }
-        });
+     *Citiation: Reference:
+      *  https://stackoverflow.com/questions/31445156
+      *  Under section - "You can invoke camera Activity by adding these lines in your code :"
+    private static Uri getOutputMediaFileUri(int type) {
+        return Uri.fromFile(getOutputMediaFileUri(type));
     }
+
 */
 
+
+/*
     public void onClickTakePhoto(View view) {
         Intent takePhotoIntent = new Intent(MediaStore.INTENT_ACTION_STILL_IMAGE_CAMERA);
-        m_file = new File(Environment.getExternalStorageDirectory(), "myImage.jpg");
+       File m_file = new File(Environment.getExternalStorageDirectory(), "Pictures");
         m_ouputFileUri = Uri.fromFile(m_file);
 
         if(takePhotoIntent.resolveActivity(getPackageManager()) !=null){
@@ -148,6 +119,9 @@ public void addListenerOnSpinner() {
             startActivity(takePhotoIntent);
         }
     }
+    */
+
+
 
     public void openEmail(View view) {
         Intent openEmailIntent = new Intent(Intent.ACTION_SEND);
@@ -169,15 +143,6 @@ public void addListenerOnSpinner() {
 
     }
 
-/*
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-            Log.i(TAG, "Enteres onActivityResult()");
-        //Checking request:
-        if(requestCode == RETURN_IMAGE)
-            if(requestCode == RESULT_OK) {
 
 
-        }
-    }
-*/
 }
